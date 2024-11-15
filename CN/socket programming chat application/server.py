2a38f5837df4199821b621719adfe0c2eeecf775
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2024-11-15 21:08:08 +0530

    socket programming basic chat app implemented

--- CN/socket programming chat application/server.py
+++ CN/socket programming chat application/server.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST='192.168.1.9'
-PORT=50000
+PORT=20000
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
@@ -14,18 +14,21 @@ print(f"Server is listening on {HOST}:{PORT}...")
 conn, addr = server_socket.accept()
 print(f"Connected by {addr}")
 
+conn.sendall(b"hello client server here")
 
 
 with conn:
     while True:
+        
         # Receive data from the client
         data = conn.recv(1024)
-        if not data:
+        if not data or data.decode()=="bye":
+            print("server has left communication")
+            conn.sendall(b"bye client")
             break
         print(f"Received from client: {data.decode()}")
 
-
-        conn.sendall(b"Hello, client! Message received.")
-
+        message=input("enter message for client:")
+        conn.sendall(message.encode())
 # Close the server socket
 server_socket.close()

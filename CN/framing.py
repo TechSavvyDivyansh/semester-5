d82a435836c60dacbc4b4c7c552efd6a6a94c613
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2024-11-18 12:56:55 +0530

    bit stuffying done

--- CN/framing.py
+++ CN/framing.py
@@ -30,11 +30,41 @@ def byte_stuffing():
 
 
 def bit_stuffing():
-    pass
+    data = list(input("Enter data: "))
+    c = 0
+    i = 0
+
+    while i < len(data):
+        if data[i] == '1':  
+            c += 1
+        else:
+            c = 0                           # Reset the counter if the current bit is not '1'
+
+        if c == 6:  
+            # If six consecutive '1's are found
+            
+            data.insert(i, '0')             # Insert '0' before the 6th '1'
+            c = 0                           # Reset the counter after stuffing
+            i += 1                          # Skip the inserted '0' to avoid infinite loop
+        
+        i += 1                              # Move to the next bit
+
+    return ''.join(data)                    # Join the list to form a string
 
 
-# transmitted_message_character_count=character_count()
+
+print("character count:")
+transmitted_message_character_count=character_count()
+print("transmitted message character count:"+transmitted_message_character_count)
+
+
+print("byte stuffing:")
 transmitted_message_byte_stuffing=byte_stuffing()
+print("transmitted message byte stuffing:"+transmitted_message_byte_stuffing)
 
-# print(data_transmitted)
-print(transmitted_message_byte_stuffing)
+
+print("bit stuffing:")
+transmitted_message_bit_stuffing=bit_stuffing()
+print("transmitted message bit stuffing:"+transmitted_message_bit_stuffing)
+
+
